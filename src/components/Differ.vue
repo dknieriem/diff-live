eab456e04db2219b4d41d58753f2ddc64c6d0917
Donald Knieriem <donald.knieriem@temple.edu>
2021-01-25 15:06:47 -0500

    tailwindcss init

---
+++ src/components/Differ.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="grid grid-cols-2">
+    <div class="relative">
+      <h2>File 1</h2>
+    </div>
+    <div class="relative">
+      <h2>File 2</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    file1: String,
+    file2: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
